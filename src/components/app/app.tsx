190a1055f9c9fc3884bc06955dfcde968b42cdb2
heyhurricane <dinohka@mail.ru>
2023-12-17 23:21:53 +0400

    Начинаем программировать

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!!!</p>;
+import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+
+type AppScreenProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppScreenProps): JSX.Element {
+  return (
+    <WelcomeScreen offersCount={offersCount} />
+  );
 }
 
 export default App;
