0ea6d8ebbdbe0c5d00801a3657ab8c5961efb9e9
heyhurricane <dinohka@mail.ru>
2023-12-26 23:21:33 +0400

    add router

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,41 @@
 import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route';
 
 type AppScreenProps = {
   offersCount: number;
-}
+};
 
-function App({offersCount}: AppScreenProps): JSX.Element {
+function App({ offersCount }: AppScreenProps): JSX.Element {
   return (
-    <WelcomeScreen offersCount={offersCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main}
+          element={<WelcomeScreen offersCount={offersCount} />}
+        />
+        <Route path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route path={AppRoute.Favorites}
+          element={
+            <PrivateRoute hasAccess={false}>
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRoute.Room}
+          element={<OfferScreen />}
+        />
+        <Route path='*'
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
